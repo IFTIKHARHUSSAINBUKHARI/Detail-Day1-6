516788a526354b7dd23a56344a9682dab182e399
IFTIKHARHUSSAINBUKHARI <iftikharhussainbukhari@gmail.com>
2025-02-08 21:00:17 +0500

    Add files via upload

---
+++ Day6/src/app/payment/page.tsx
@@ -0,0 +1,12 @@
+import StripePayment from '@/components/stripepayment'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <StripePayment></StripePayment>
+    </div>
+  )
+}
+
+export default page
